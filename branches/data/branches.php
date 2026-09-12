c0ae1fac7a01432c5e6b0f2ba962703bfc39f6af
Satnam Singh <singhkhowal@gmail.com>
2026-09-13 02:11:53 +0530

    Restore verified branch data module

---
+++ branches/data/branches.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Verified physical branch records only.
+ * Do not add service-area/location SEO pages here unless Forsk Coding School
+ * has a real operating branch and the details have been confirmed.
+ */
+return [
+    'jaipur-shyam-nagar' => [
+        'name' => 'Forsk Coding School - Shyam Nagar, Jaipur',
+        'city' => 'Jaipur',
+        'area' => 'Shyam Nagar',
+        'state' => 'Rajasthan',
+        'postal_code' => '302019',
+        'address' => 'F1, Forsk Coding School, New Sanganer Rd, F Block, Shyam Nagar, Jaipur, Rajasthan 302019',
+        'email' => 'info@forskcodingschool.com',
+        'contact_url' => '/contact/',
+        'canonical' => 'https://forskcodingschool.com/branches/jaipur-shyam-nagar/',
+        'status' => 'verified',
+    ],
+];
