4a51f04a6a73b92c25230f97a6da2a6fcd0e2880
Satnam Singh <singhkhowal@gmail.com>
2026-09-19 15:00:53 +0530

    feat: add Forsk social page includes/social-platforms.php

---
+++ includes/social-platforms.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Canonical public social profile configuration for Forsk Coding School.
+ * Keep verified handles/URLs here so all internal social pages stay consistent.
+ */
+return [
+    'instagram' => [
+        'name' => 'Instagram',
+        'short_name' => 'Instagram',
+        'handle' => '@forskcodingschool',
+        'url' => 'https://www.instagram.com/forskcodingschool/',
+        'embed' => 'instagram',
+        'embed_url' => 'https://www.instagram.com/forskcodingschool/embed/',
+        'description' => 'Follow Forsk Coding School on Instagram for coding tips, course updates, student-focused content, reels and technology learning posts.',
+    ],
+    'facebook' => [
+        'name' => 'Facebook',
+        'short_name' => 'Facebook',
+        'handle' => 'ForskCodingSchool',
+        'url' => 'https://www.facebook.com/ForskCodingSchool',
+        'embed' => 'facebook',
+        'description' => 'View Forsk Coding School on Facebook for institute updates, learning content, announcements and community posts.',
+    ],
+    'youtube' => [
+        'name' => 'YouTube',
+        'short_name' => 'YouTube',
+        'handle' => '@ForskCodingSchoolJaipur',
+        'url' => 'https://www.youtube.com/@ForskCodingSchoolJaipur',
+        'embed' => 'youtube',
+        'description' => 'Watch Forsk Coding School on YouTube for long-form coding videos, tutorials, course guidance and Shorts.',
+    ],
+    'linkedin' => [
+        'name' => 'LinkedIn',
+        'short_name' => 'LinkedIn',
+        'handle' => 'forskcodingschool',
+        'url' => 'https://www.linkedin.com/company/forskcodingschool',
+        'embed' => 'linkedin',
+        'description' => 'Connect with Forsk Coding School on LinkedIn for professional updates, technical learning posts and career-focused content.',
+    ],
+    'twitter' => [
+        'name' => 'X / Twitter',
+        'short_name' => 'X / Twitter',
+        'handle' => '',
+        'url' => '',
+        'embed' => 'unconfigured',
+        'description' => 'The official Forsk Coding School X / Twitter handle is not currently linked in the verified website social references.',
+    ],
+    'blogger' => [
+        'name' => 'Blogger',
+        'short_name' => 'Blogger',
+        'handle' => 'forskcodingschool',
+        'url' => 'https://forskcodingschool.blogspot.com/',
+        'embed' => 'blogger',
+        'description' => 'Read Forsk Coding School updates and learning posts on the official Blogger profile.',
+    ],
+];
